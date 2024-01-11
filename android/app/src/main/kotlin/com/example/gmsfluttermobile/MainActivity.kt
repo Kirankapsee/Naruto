ccdcbf7d4bd5f138552e658f56dd645f0da6893a
Kirankapsee <139834218+Kirankapsee@users.noreply.github.com>
2024-01-11 13:25:22 +0530

    Add files via upload

---
+++ android/app/src/main/kotlin/com/example/gmsfluttermobile/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.furationtechsolutions.reserve1st
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ android/app/src/main/kotlin/com/furationtechsolutions/gmsfluttermobile/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.furationtechsolutions.gmsfluttermobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
